a065e6b9e15ec771ab1306e73b8293d04f97625b
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-09-16 15:25:02 +0100

    " "

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * puts - Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
-*/
-int  main(void)
-{
-    puts("\"programming is like building a multilingual puzzle" /n);
+ */
+int main(void)
+	{
+    puts("\"programming is like building a multilingual puzzle");
     return (0);
     }
