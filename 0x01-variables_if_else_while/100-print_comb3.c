f500aa1c84efc6afd1c5865dc15dbd40c52cad8a
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-09-17 23:26:08 +0100

    " "

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+*main - main function
+*
+*Return: 0
+*/
+int main(void)
+{
+int x, e;
+
+for (x = '0'; x <= '9'; ++x)
+{
+for (e = x; e <= '9'; ++e)
+{
+if (x != e)
+{
+if (x != '0' || e != '1')
+{
+putchar(',');
+putchar(' ');
+}
+putchar(x);
+putchar(e);
+}
+}
+}
+
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+*main - maine function
+*Return: 0
+*/
+int main(void)
+{
+int x, e, z;
+
+for (x = '0'; x <= '9'; ++x)
+{
+for (e = x; e <= '9'; ++e)
+{
+for (z = e; z <= '9'; ++z)
+{
+if (x != e && x != z && e != z)
+{
+if (x != '0' || e != '1' || z != '2')
+{
+putchar(',');
+putchar(' ');
+}
+putchar(x);
+putchar(e);
+putchar(z);
+}
+}
+}
+}
+
+putchar('\n');
+return (0);
+}
