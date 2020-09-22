053be457ac35c1b6b97da57c83c93ebffec5b996
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-09-22 11:39:36 +0100

    " "

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,19 @@
-include "holberton.h"
+#include "holberton.h"
 /**
-*_islower - 0
-*@c: an intiger
+*_isalpha - 0
+*@c: integer
 *Return: either 1 or 0
 */
-int _islower(int c)
+int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 
 
