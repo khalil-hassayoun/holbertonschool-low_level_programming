62a6c96073e53aceb54de00f4fdadc4eb856dd48
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-09-22 11:09:24 +0100

    " "

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+*print_alphabet - 0
+*Return: nothing
+*/
+void print_alphabet(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; ++i)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
