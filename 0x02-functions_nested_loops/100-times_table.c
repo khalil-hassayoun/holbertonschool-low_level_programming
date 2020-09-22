823ebf332907b885e75104392d561b611432d821
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-09-22 22:36:58 +0100

    " "

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,71 @@
 #include "holberton.h"
 /**
-* print_times_table - main function
-* @n: char
-*
-* Return: 1
+*time - 0
+*@i: int
+*@j: int
+*/
+void time(int i, int j)
+{
+	if (i * j > 99)
+	{
+		if (j != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar((i * j) / 100 + '0');
+		_putchar(((i * j) / 10) % 10 + '0');
+		_putchar((i * j) % 10 + '0');
+	}
+	else if (i * j > 9)
+	{
+		if (j != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar((i * j) / 10 + '0');
+		_putchar((i * j) % 10 + '0');
+	}
+	else
+	{
+		if (j != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar((i * j) + '0');
+	}
+}
+/**
+*table - 0
+*@n: int
+*/
+void table(int n)
+{
+	int i, j;
+
+	for (i = 0; i <= n; ++i)
+	{
+		for (j = 0; j <= n; ++j)
+		{
+			time(i, j);
+		}
+		_putchar('\n');
+	}
+}
+/**
+*print_times_table - 0
+*@n: integer
+*Return: nothing
 */
 void print_times_table(int n)
 {
-int i, j, r;
-
-if (n < 0 || n > 15)
-return;
-for (i = 0 ; i <= n ; i++)
-{
-_putchar('0');
-if (n != 0)
-_putchar(',');
-for (j = 1 ; j <= n ; j++)
-{
-r = i * j;
-if (r < 10)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar(r + '0');
-}
-else if (r < 100)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar((r / 10) + '0');
-_putchar((r % 10) + '0');
-}
-else
-{
-_putchar(' ');
-_putchar((r / 100) + '0');
-_putchar(((r % 100) / 10) + '0');
-_putchar(((r % 100) % 10) + '0');
-}
-if (j < n)
-{
-_putchar(',');
-}
-}
-_putchar('\n');
-}
+	if (n <= 15 && n >= 0)
+	{
+		table(n);
+	}
 }
