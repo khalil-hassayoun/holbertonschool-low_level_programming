bdfcdbccf5cc8207af6d7955a283d6418abcf844
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-09-22 11:50:13 +0100

    " "

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+*print_sign - 0
+*@n: integer
+*Return: 1 , 0 or -1
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+
+if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+_putchar('0');
+return (0);
+}
