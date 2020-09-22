ea520350833feeba329bb334619b67e742763047
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-09-22 11:22:30 +0100

    " "

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*_islower - 0
+*@c: an intiger
+*Return: either 1 or 0
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
