6d5afeb27f8a4f79631fd0dff009b7c3640210fc
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-10-01 21:59:19 +0100

    more fixes

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,22 +1,35 @@
 #include "holberton.h"
-#include <stdio.h>
-
 /**
-* rot13 - function that encodes a string using rot13.
-* @a: pointer to char
-* Return: return encoded string using rot13 from @a value
-**/
-char *rot13(char *a)
+* rot13 - function that encodes a string using rot13 method.
+* @str: string.
+* Return: string.
+*/
+char *rot13(char *str)
 {
-	int i = 0;
+	int i, j;
+	char g1[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+			 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+			 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+			 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+			 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-	while (a[i] != '\0')
+	char g2[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+			 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+			 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't',
+			 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e',
+			 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-			if ((a[i] > 'a' && a[i] < 'N') || (a[i] > 'A' && a[i] < 'N'))
-			a[i] += 13;
-			else if ((a[i] > 'm' && a[i] < 'z') || (a[i] > 'M' && a[i] < 'Z'))
-			a[i] -= 13;
-			i++;
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == g1[j])
+			{
+			str[i] = g2[j];
+			break;
+			}
+		}
 	}
-	return (a);
+
+	return (str);
 }
