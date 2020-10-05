f16ede162233441b0a3ac9afd0be74d88dce487b
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-10-05 16:11:45 +0100

    " "

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,9 +15,10 @@ char *_strchr(char *s, char c)
 	while (s[i] >= '\0')
 	{
 		if (s[i] == c)
-			return (&s[i]);
+		{	
+		return (&s[i]);
+	}
 	i++;
 	}
-	if (s[i] == c)
-	return ('\0');
+	return (0);
 }
