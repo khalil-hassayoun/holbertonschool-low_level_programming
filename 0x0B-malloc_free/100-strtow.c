eb216c157a673edc358c4db374c986735550abbb
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-10-13 15:42:55 +0100

    reallyyyyyy though

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,125 +1,64 @@
-#include <stdio.h>
-#include "holberton.h"
 #include <stdlib.h>
+#include "holberton.h"
+#include <stdio.h>
 /**
- * _strlen - returns the length of a given string
- * @s: string to check the length of
- * Return: returns the length of the string
- */
-int _strlen(char *s)
+* split_word - function
+* @str: Getting a string
+* Return: Number of words
+**/
+int split_word(char *str)
 {
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-/**
- * wc - gets a word count on a given string
- * @str: string to get word count on
- * Return: returns number of words in string
- */
-int wc(char *str)
-{
-	int i, count, inword;
-	count = 0;
-	inword = 0;
-	for (i = 0; i < _strlen(str) && str[i] != '\0'; i++)
+	int i = 0, words = 0;
+	while (str[i] != '\0')
 	{
-		if (str[i] != ' ' && str[i] != '\n' && str[i] != '\t')
-		{
-			if (inword == 0)
-			{
-				inword = 1;
-				count++;
-			}
-		}
-		else if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t')
-		{
-			if (inword == 1)
-				inword = 0;
-		}
+	if (str[i] != ' ' && str[i - 1] == ' ')
+	words++;
+	i++;
 	}
-	return (count);
+	return (words);
 }
 /**
- * _strcpy - copies an entire string to a buffer
- * @src: source string to copy
- * @dest: buffer to copy string into
- * Return: returns pointer to copied string
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i;
-	char *retval;
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = src[i];
-	retval = dest;
-	return (retval);
-}
-/**
- * _strdup - duplicates a string to a newly allocated space
- * @str: string to dupe
- * Return: duplicated string
- */
-char *_strdup(char *str)
-{
-	char *dupe;
-	dupe = malloc(_strlen(str) + 1);
-	if (dupe == NULL)
-		return (NULL);
-	_strcpy(dupe, str);
-	return (dupe);
-}
-/**
- * strtow - splits a string into seperate words and makes a 2d array
- * @str: string to split
- * Return: returns a pointer to a 2d array
- */
+* strtow - function that splites a string into words.
+* @str: Getting a string
+* Return: Array of words, without white space
+**/
 char **strtow(char *str)
 {
-	int wordcount, i, j, k, l;
-	char **array, *copy;
-
-	if (str == NULL || str[0] == '\0' || wc(str) == 0)
-		return (NULL);
-	wordcount = wc(str);
-	copy = _strdup(str);
-	if (copy == NULL)
-		return (NULL);
-	array = malloc((wordcount + 1) * sizeof(*array));
-	if (array == (NULL))
-		return (NULL);
-	i = j = l = k = 0;
-	while (str[i] != '\0' && k != wordcount)
+	int i = 0, j = 0, b = 0, c = 0, r = 0;
+	int words = 0;
+	char **p;
+		if (str == NULL)
+	return (NULL);
+	words = split_word(str);
+	p = malloc(1 + words * sizeof(char *));
+	if (p == NULL)
 	{
-		l = 0;
-		while (str[i] == ' ' && str[i] != '\0')
-			i++;
-		j = i;
-		while (copy[j] != ' ' && copy[j] != '\0')
-			j++;
-		array[k] = malloc(sizeof(char) * (j - i));
-		if (array[k] == NULL)
-		{
-			while (--k >= 0)
-			{
-				free(array[k]);
-				free(array);
-				return (NULL);
-			}
-		}
-		while (str[i] != ' ' && str[i] != '\0')
-			array[k][l++] = str[i++];
-		array[k++][l] = '\0';
-		i = j;
+	return (NULL);
 	}
-	array[wordcount] = NULL;
-	return (array);
+	i = 0, c = 0;
+	for (j = 0; j < words; j++)
+	{
+	b = 0, r = 0;
+	for (; str[i] == ' '; i++)
+	;
+	for (; str[i] != ' '; i++)
+	b++;
+	b = b + 1;
+	p[j] = malloc(b * sizeof(char));
+	if (p[j] == NULL)
+	{
+	for (; j >= 0; j--)
+	free(p[j]);
+	free(p);
+	}
+	for (; str[c] == ' '; c++)
+	;
+	for (; str[c] != ' ' && r <= b; c++)
+	{
+	p[j][r] = str[c];
+	r++;
+	}
+	p[j][r] = '\0';
+	}
+	return (p);
 }
