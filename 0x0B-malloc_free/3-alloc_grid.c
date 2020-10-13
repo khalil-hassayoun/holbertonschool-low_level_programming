c75c7e20d0a3103c5a0c860b26757bb7d12836d7
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-10-13 14:26:59 +0100

    " "

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,48 +1,36 @@
-#include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 /**
- * alloc_grid - allocate a grid
- * @width: width of the grid
- * @height: height of the grid
- * Return: pointer to grid
- */
+* alloc_grid - function that returns a pointer
+* @width: width of the grid
+* @height: height of the grid
+* Return: return the value 0 for each element of the grid
+**/
 int **alloc_grid(int width, int height)
 {
+	int i = 0, b = 0;
 	int **p;
-	int i, j;
+
 	if (width <= 0 || height <= 0)
-	{
 		return (NULL);
-	}
-	i = 0;
-	p = malloc(width * sizeof(p));
+	p = malloc(height * sizeof(int *));
 	if (p == NULL)
-	{
 		return (NULL);
-	}
 	while (i < height)
 	{
-		p[i] = malloc(height * sizeof(p[i]));
+		b = 0;
+		p[i] = malloc(width * sizeof(int));
 		if (p[i] == NULL)
 		{
-			for (j = 0; j < i; ++j)
-			{
-				free(p[j]);
-			}
+			for (; i >= 0; i--)
+				free(p[i]);
 			free(p);
 			return (NULL);
 		}
-		i++;
-	}
-	i = 0;
-	while (i < width)
-	{
-		j = 0;
-		while (j < height)
+		while (b < width)
 		{
-			p[j][i] = 0;
-			j++;
+			p[i][b] = 0;
+			b++;
 		}
 		i++;
 	}
