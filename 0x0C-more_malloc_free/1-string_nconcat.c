32771ae9b558cef1fe70c641e993a9b711dfe9b1
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-10-14 21:18:18 +0100

    " "

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,9 +1,7 @@
 #include <stdlib.h>
 
 /**
- * _strlen - returns the length of a given string
- *
- *
+ * length - returns the length of a given string
  * @s: string to check the length of
  * Return: returns the length of the string
  */
