6a86bf963917b105c2a5e01148c26f52530eb1fa
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-10-14 21:41:15 +0100

    " "

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
-
 /**
  * _realloc - moves data in pointer to new area with different space allocated
  * @ptr: pointer to move data from
@@ -11,27 +11,20 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *newspace;
-	unsigned int i;
 
 	if (new_size == old_size)
-		return (ptr);
-	if (ptr == NULL)
-	{
-		newspace = malloc(new_size);
-		if (newspace == NULL)
-			return (NULL);
-		return (newspace);
-	}
+	return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
 	newspace = malloc(new_size);
-	if (newspace == NULL)
-		return (NULL);
-	for (i = 0; i < old_size && i < new_size; i++)
-		newspace[i] = ((char *)ptr)[i];
-	free(ptr);
-	return (newspace);
+free(ptr);
+return (newspace);
 }
