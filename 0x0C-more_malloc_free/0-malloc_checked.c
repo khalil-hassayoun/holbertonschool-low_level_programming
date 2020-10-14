dabcc1624f864f63e92e10bc45326850006fbb38
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-10-14 21:02:46 +0100

    " "

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,17 +1,14 @@
 #include "holberton.h"
 #include <stdlib.h>
-
 /**
  * malloc_checked - mallocs memory asked, exits with code 98 on failure
  * @b: var
  * Return: 0
  */
-
 void *malloc_checked(unsigned int b)
 {
-	void *pointer = malloc(b);
-
-	if (pointer == NULL)
-		exit(98);
-	return (pointer);
+void *p = malloc(b);
+if (p == NULL)
+exit(98);
+return (p);
 }
