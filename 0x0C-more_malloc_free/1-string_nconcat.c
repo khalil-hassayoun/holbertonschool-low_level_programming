e1a5a02439d233b9c6bc7b837caaf6d71d3d4cbc
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-10-14 21:04:18 +0100

    " "

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,51 @@
 #include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
-
 /**
-* string_nconcat - function that concatenates two strings
-* @s1: First string
-* @s2: Second string
-* @n: Unsigned integer
-* Return: concatenated string
- **/
-
+ * string_nconcat - 0
+ * @s1: string
+ * @s2: string
+ * @n: char number
+ * Return: string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i = 0, j = 0, k = 0, l = 0;
-char *p;
+	char *p;
+	unsigned int i, j, t, r;
 
 	if (s1 != NULL)
 	{
-	while (s1[i] != '\0')
-		i++;
+		for (i = 0; s1[i] != '\0' ; ++i)
+			;
 	}
+	else
+		i = 0;
 	if (s2 != NULL)
 	{
-	while (s2[j] != '\0')
-		j++;
+		for (j = 0; s2[j] != '\0' ; ++j)
+			;
 	}
+	else
+		r = 0;
 	if (n >= j)
-		n = j;
-	p = malloc(i + n + 1 * sizeof(char));
+	{
+		p = malloc((i + j) * sizeof(s1));
+		r = j;
+	}
+	else
+	{
+		p = malloc((i + n) * sizeof(s1));
+		r = n;
+	}
 	if (p == NULL)
 		return (NULL);
-	for (k = 0; k < i; k++)
+	for (t = 0; t < (i + r); ++t)
 	{
-		p[k] = s1[l];
-		l++;
+		if (t < i)
+			p[t] = s1[t];
+		else
+			p[t] = s2[t - i];
 	}
-	for (l = 0; l < n; l++)
-	{
-		p[k] = s2[l];
-		k++;
-	}
-	p[k] = '\0';
+	p[t + 1] = '\0';
 	return (p);
 }
