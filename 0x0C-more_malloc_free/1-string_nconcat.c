d8709968c0b386a5017b8ae71e0619daa9499cc4
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-10-14 21:16:59 +0100

    " "

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,9 +16,7 @@ unsigned int length(char *s)
 }
 
 /**
- * string_nconcat - concats two strings, using n chars from s2,
- * or all if n is over s2
- *
+ * string_nconcat - concats two strings, using n chars from s2
  * @s1: string one
  * @s2: string two
  * @n: bytes of s2 to use
@@ -33,15 +31,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if ((int) n > _strlen(s2))
-		n = _strlen(s2);
-	newstring = malloc(_strlen(s1) + n + 1);
+	if ((int) n > lenght(s2))
+		n = length(s2);
+	newstring = malloc(length(s1) + n + 1);
 	if (newstring == NULL)
 		return (NULL);
-	for (i = 0, j = 0; s1[i] != '\0'; i++, j++)
-		newstring[j] = s1[i];
+	for (i = 0, j = 0; s1[j] != '\0'; i++, j++)
+		newstring[j] = s1[j];
 	for (i = 0; i != n; i++, j++)
-		newstring[j] = s2[i];
+		newstring[i] = s2[j];
 	newstring[j] = '\0';
 	return (newstring);
 }
