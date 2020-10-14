3834b7b80e626cbe51779490e9d384e176f0157d
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-10-14 20:56:55 +0100

    " "

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,15 +3,14 @@
 
 /**
  * malloc_checked - mallocs memory asked, exits with code 98 on failure
- * @b: memory required
- * Return: pointer to memory requested
+ * @b: var
+ * Return: 0
  */
 
 void *malloc_checked(unsigned int b)
 {
-	void *pointer;
+	void *pointer = malloc(b);
 
-	pointer = malloc(b);
 	if (pointer == NULL)
 		exit(98);
 	return (pointer);
