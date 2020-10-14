a90d1eaee200381825b067df0ef9537adc5d247d
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-10-14 21:26:02 +0100

    " "

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,24 +1,25 @@
 #include "holberton.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * _calloc - mallocs assigned memory and sets to zero
- * @nmemb: number of memory spaces
- * @size: size of nmemb
- * Return: returns pointer to allocated space, or NULL on failure
+ * _calloc - allocate an array
+ * @nmemb:int
+ * @size:int
+ * Return:void
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
-	char *s;
+char *s;
+unsigned int i;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	s = malloc(nmemb * size);
-	if (s == NULL)
-		return (NULL);
-	for (i = 0; i != size; i++)
-		*(s + (size * i)) = 0;
+if (size == 0 || nmemb == 0)
+return (NULL);
+s = malloc(size * nmemb);
+if (s == NULL)
+return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+	{
+		s[i] = '\0';
+	}
 	return (s);
 }
