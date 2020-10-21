861e946c0995e11ca3ca68e352e092e9060f1763
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-10-21 19:19:13 +0100

    " "

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,18 +3,18 @@
 /**
  * array_iterator - function to call another function against every element in
  * an array
- *
  * @array: array to work again
  * @size: size of array
  * @action: function to call
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+size_t i;
 
-	i = 0;
-	while (i < (int)size)
+i = 0;
+if (action != NULL && array != NULL)
 	{
+		for (i = 0; i < size; i++)
 		(*action)(array[i++]);
 	}
 }
