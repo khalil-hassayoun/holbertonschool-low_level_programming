974a3a38aff0a984763c77e2af8b1683cc2ab835
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-10-30 21:55:35 +0100

    " "

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,15 +7,14 @@
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
+	list_t *temp, *check;
 
 	temp = head;
 	while (head != NULL)
 	{
-		temp = head;
-		head = head->next;
+		head = temp->next;
 		free(temp->str);
 		free(temp);
+		temp = check;
 	}
-	free(head);
 }
