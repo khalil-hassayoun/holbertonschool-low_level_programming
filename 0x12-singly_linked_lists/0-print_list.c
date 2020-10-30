2435a3f1c911cb1f5d210ee39ed5017aece3ade8
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-10-30 21:51:27 +0100

    " "

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,25 @@
 #include "lists.h"
-
 /**
  * print_list - returns number of elements in linked list
  *
  * @h: head of const list being counted
  * Return: returns number of elements in list
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t len = 0;
 	int i;
-	const list_t *temp;
+	const list_t *c = h;
 
-	i = 0;
-	temp = h;
-	while (temp != NULL)
+	for (i = 0; c; i++)
 	{
-		printf("[%d] %s\n", temp->len, temp->str);
-		temp = temp->next;
-		i++;
+		if (c->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", c->len, c->str);
+	c = c->next;
+	len++;
 	}
-	return (i);
+return (len);
 }
