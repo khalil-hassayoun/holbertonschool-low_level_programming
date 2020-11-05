72507ff11182b4273729f1525390c77f40b47d1d
khalil-hassayoun <khalil.hassayoun@hotmail.fr>
2020-11-05 22:02:07 +0100

    " "

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,62 +1,13 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <limits.h>
-
 /**
- * print_binary - prints a long int out as a binary number
+ * print_binary - function that prints the binary.
  *
- * @n: int to print
+ * @n: unsigned int.
  */
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int i, cntr;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	if (n == 1)
-	{
-		_putchar('1');
-		return;
-	}
-	i = 2;
-	cntr = 0;
-	while (i <= n && cntr < 62)
-	{
-		i <<= 1;
-		cntr++;
-	}
-	if (cntr != 62)
-		i >>= 1;
-	while (i != 0)
-	{
-		printf("i: %lu\n", i);
-		printf("i & n: %lu\n", (i & n));
-		if ((i & n) != 0)
-			cntr++;
-//			_putchar('1');
-		else
-			cntr++;
-//			_putchar('0');
-		i >>= 1;
-	}
-}
-
-
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+	if (n > 1)
+	print_binary(n >> 1);
+_putchar((n & 1) + '0');
 }
